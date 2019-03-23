51d28a2fa653149eede418f4c0c084440a83e830
arpi <gal.arpad.laszlo@gmail.com>
2019-03-23 20:37:04 +0100

    tutor 2 chagnes

---
+++ MyFirstEpiserverSite/Business/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MyFirstEpiserverSite.Business
+{
+  [InitializableModule]
+  public class BundleConfig : IInitializableModule
+  {
+    public void Initialize(InitializationEngine context)
+    {
+      if (context.HostType == HostType.WebApplication)
+      {
+        RegisterBundles(BundleTable.Bundles);
+      }
+    
+    }
+
+    private static void RegisterBundles(BundleCollection bundles)
+    {
+      bundles.Add(new ScriptBundle("~/bundles/jquery.js").Include(
+        "~/Scripts/jquery-{version}.js"));
+
+      bundles.Add(new ScriptBundle("~/bundles/bootstrap.js").Include(
+        "~/Scripts/bootsrap.js"
+       ,"~/Scripts/respond.js"));
+
+      bundles.Add(new StyleBundle("~/Content/style.css").Include(
+        "~/Content/bootstrap.css"));
+
+      BundleTable.EnableOptimizations = false;
+    }
+
+    public void Uninitialize(InitializationEngine context)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
