8890cd4e79ae69c8531e8f7deb95888edc25ed7c
arpi <gal.arpad.laszlo@gmail.com>
2019-03-23 17:17:50 +0100

    create bootstrap page

---
+++ MyFirstEpiserverSite/Controllers/StartPageController.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Web.Mvc;
+using MyFirstEpiserverSite.Models.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstEpiserverSite.Controllers
+{
+    public class StartPageController : PageController<StartPage>
+  {
+        // GET: StartPage
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstEpiserverSite/Models/Pages/StartPage.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstEpiserverSite.Models.Pages
+{
+  [ContentType]
+  public class StartPage : PageData
+  {
+  }
+}
